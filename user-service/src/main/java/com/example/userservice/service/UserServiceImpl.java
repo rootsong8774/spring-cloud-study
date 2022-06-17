ba03af4d9b946003caa58909c90ec92e7529e797
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-06-17 17:05:57 +0900

    RestTemplate 을 사용하여 User Microservice 와 Order Microservice 를 연결

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,15 +6,21 @@ import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +28,8 @@ public class UserServiceImpl implements UserService {
     
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RestTemplate restTemplate;
+    private final Environment environment;
     
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -46,9 +54,16 @@ public class UserServiceImpl implements UserService {
         }
         
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
-        
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
+
+//        List<ResponseOrder> orders = new ArrayList<>();
+        /* Using as rest template */
+        String orderUrl = String.format(
+            Objects.requireNonNull(environment.getProperty("order_service.url")), userId);
+        ResponseEntity<List<ResponseOrder>> orderListResponse = restTemplate.exchange(orderUrl,
+            HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+            });
+        List<ResponseOrder> orderList = orderListResponse.getBody();
+        userDto.setOrders(orderList);
         
         return userDto;
     }
@@ -61,7 +76,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto getUserDetailsByEmail(String email) {
         UserEntity userEntity = userRepository.findByEmail(email);
-    
+        
         if (userEntity == null) {
             throw new UsernameNotFoundException(email);
         }
